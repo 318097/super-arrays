d00a60af1e3b97f23898f412d7793936cb3d715d
Mehul Lakhanpal <318097@gmail.com>
2022-11-16 22:46:38 +0530

    Refactor (Toddle backup)

--- src/SuperArray.ts
+++ src/SuperArray.ts
@@ -1,6 +1,23 @@
 class SuperArray {
-  data = [];
+  arr = [];
+  constructor() {
 
+  }
+
+  crux = (cmd, payload) => {
+    switch (cmd) {
+      case "add":
+        return [...this.arr, payload];
+      case "update":
+        return this.arr.map((item: any) =>
+          item.id === payload.id ? { ...item, ...payload } : item
+        );
+      case "read":
+        return this.arr.find((item: any) => item.id === payload.id);
+      case "delete":
+        return this.arr.filter((item: any) => item.id !== payload.id) || [];
+    }
+  };
 }
 
-export default SuperArray
+export default SuperArray;
